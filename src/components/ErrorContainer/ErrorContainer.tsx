95c09382b23021abbb0301216edbaf4a08e86727
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-05-24 13:42:29 +0200

    (feat): styles error page

--- src/components/ErrorContainer/ErrorContainer.tsx
+++ src/components/ErrorContainer/ErrorContainer.tsx
@@ -1,7 +1,21 @@
 import { FunctionComponent } from 'react';
 
+// others
+import Background from '../../assets/images/backgrounds/error.svg';
+
+// styles
+import './error-container.scss';
+
 const ErrorContainer: FunctionComponent<{}> = () => {
-  return <div className="Error">Error</div>;
+  return (
+    <div className="Error">
+      <img
+        alt="background-error"
+        className="Error__background"
+        src={Background}
+      />
+    </div>
+  );
 };
 
 export default ErrorContainer;

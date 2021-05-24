33c219715017f8fcfc7cce82a6e3529d77faa17b
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-05-24 11:34:55 +0200

    Updates

---
+++ static/js/main.ba30e84d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgentleman-messenger"]=this["webpackJsonpgentleman-messenger"]||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var s=t(0),c=t.n(s),r=t(3),a=t.n(r),i=(t(8),t(1));var o=function(){return Object(i.jsx)("div",{className:"App"})};a.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
+//# sourceMappingURL=main.ba30e84d.chunk.js.map

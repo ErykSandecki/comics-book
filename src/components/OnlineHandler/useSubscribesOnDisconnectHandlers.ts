731e5d8c8cfa288c2f1d2a6d2a06922d8f5ca4b2
ErykSandecki <eryk.sandecki@gmail.com>
2021-06-08 17:39:29 +0200

    (fix): fix pending styles

--- src/components/OnlineHandler/useSubscribesOnDisconnectHandlers.ts
+++ src/components/OnlineHandler/useSubscribesOnDisconnectHandlers.ts
@@ -22,7 +22,7 @@ const useSubscribesDisconnectHandlers = (
       .onDisconnect()
       .set(false);
 
-  const subscribeTypings = () => null;
+  const subscribeTypings = () => 
     getRefDatabase([DatabaseColumns.typings])
       .onDisconnect()
       .set(getTypingsWithRemovedItem(typings, selectedProfileId));

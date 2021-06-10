0b7d0c5c2d601650ea9bd3a13b483d8b490047e6
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-06-10 21:58:16 +0200

    (feat): upadte modal update

--- src/components/ModalNewUpdates/constants.ts
+++ src/components/ModalNewUpdates/constants.ts
@@ -1,13 +1,5 @@
 export const UPDATE_DETAILS = [
-  'Application access by password',
-  'Ability to create profiles',
-  'Ability to create channels',
-  'Status of active brothers-in-law',
-  'Application adapted to all devices',
-  'Ability to write on the channel',
-  'Status on the number of active brothers-in-law',
-  'Ability to send messages',
-  'Determination of date of last message',
-  'The current status of a person who is currently writing on a channel',
+  'Parsing links embedded in messages',
+  'Parsing links embedded in messages',
 ];
-export const CURRENT_VERSION = '1.0.0';
+export const CURRENT_VERSION = '1.1.0';

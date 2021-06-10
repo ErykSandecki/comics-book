fcb4ecbf3581a323a7397b65799a3bfa3a14506b
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-06-10 21:56:44 +0200

    (feat): add parser to url links

--- src/components/Chat/ChatInput/services.ts
+++ src/components/Chat/ChatInput/services.ts
@@ -1,10 +1,18 @@
 import { TTyping } from '../../../store/typings/types';
 
 export const getPreparedContent = (content: string) =>
-  content.replaceAll(
+  urlifyContent(content).replaceAll(
     '<div><br></div>',
     `<div class="ChatMessages__break-line"><br></div>`
   );
+
+export const urlifyContent = (content: string): string => {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  return content.replaceAll(
+    urlRegex,
+    (url) => '<a href="' + url + '" target="_blank">' + url + '</a>'
+  );
+};
 
 export const getTypingsWithRemovedItem = (
   typings: Array<TTyping>,

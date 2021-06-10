a37f18ceddbea50f14258e62bd74bd02f3b63205
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-06-10 21:58:55 +0200

    (feat): upadte modal update

--- src/components/ModalNewUpdates/constants.ts
+++ src/components/ModalNewUpdates/constants.ts
@@ -1,5 +1,5 @@
 export const UPDATE_DETAILS = [
   'Parsing links embedded in messages',
-  'Parsing links embedded in messages',
+  'When you click on a link a new tab opens',
 ];
 export const CURRENT_VERSION = '1.1.0';

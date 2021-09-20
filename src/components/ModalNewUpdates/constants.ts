3a0f976b4d5fac0a73eccd2a2231a8e0bf5a246a
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-09-20 22:15:45 +0200

    Update details for update

--- src/components/ModalNewUpdates/constants.ts
+++ src/components/ModalNewUpdates/constants.ts
@@ -2,5 +2,6 @@ export const UPDATE_DETAILS = [
   'Support for color text form Telegram',
   'Resolved problem with width text',
   'Http parser resolved for numbers',
+  'Shorten Loader, removed animation welcome gentleman',
 ];
 export const CURRENT_VERSION = '1.2.0';

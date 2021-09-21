371925701a8c4206426a5950c936b5a75730938a
ErykSandeckiIntive <eryk.sandecki@intive.com>
2021-09-21 17:38:48 +0200

    update description for updates

--- src/components/ModalNewUpdates/constants.ts
+++ src/components/ModalNewUpdates/constants.ts
@@ -3,5 +3,6 @@ export const UPDATE_DETAILS = [
   'Resolved problem with width text',
   'Http parser resolved for numbers',
   'Shorten Loader, removed animation welcome gentleman',
+  'Problem with scrolling on panel with channels & profiles for mobile on chrome',
 ];
 export const CURRENT_VERSION = '1.2.0';
